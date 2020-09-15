e4ef0b31691556e65a0ec4fd01814e4ebd6b4e12
AndrewB4y <andresbaymon@gmail.com>
2020-09-15 01:34:16 +0000

    Adding double quoting to the output csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -21,20 +21,22 @@ if __name__ == "__main__":
     req_todos = 'https://jsonplaceholder.typicode.com/todos'
     r_us = requests.get(req_users.format(employee_id))
     r_todos = requests.get(req_todos)
-
     user = r_us.json()
     todos = r_todos.json()
     my_dict = {}
     content = []
-    fields = ["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"]
+    fields = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
     for a_todo in todos:
         if a_todo.get("userId") == int(employee_id):
-            content.append({"USER_ID": a_todo.get('userId'),
-                            "USERNAME": user.get('name'),
-                            "TASK_COMPLETED_STATUS": a_todo.get('completed'),
-                            "TASK_TITLE": a_todo.get('title')})
+            content.append({"USER_ID": str(a_todo.get('userId')),
+                            "USERNAME": str(user.get('name')),
+                            "TASK_COMPLETED_STATUS":
+                            str(a_todo.get('completed')),
+                            "TASK_TITLE": str(a_todo.get('title'))})
 
     with open('{}.csv'.format(employee_id), 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer = csv.DictWriter(csvfile,
+                                fieldnames=fields,
+                                quoting=csv.QUOTE_ALL)
         for data in content:
             writer.writerow(data)

fcd8c1418e76681e14cd081bea5145cbac70500d
AndrewB4y <andresbaymon@gmail.com>
2020-09-17 20:10:16 +0000

    Adding documentation for the method of file 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,11 @@ import json
 
 
 def number_of_subscribers(subreddit):
+    """
+    number_of_subsribers
+    queries the Reddit API and returns the number of subscribers
+    for a given subreddit
+    """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent':
                'Mozilla/5.0 (Windows NT 10.0; Win64; '

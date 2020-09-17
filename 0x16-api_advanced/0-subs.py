a7fa3c9eaaed3a69adf860cd520623b337d24e8d
AndrewB4y <andresbaymon@gmail.com>
2020-09-17 20:07:39 +0000

    Creating the project 0x16 and adding file 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" 0-subs module """
+
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent':
+               'Mozilla/5.0 (Windows NT 10.0; Win64; '
+               'x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+               'Chrome/85.0.4183.102 Safari/537.36'}
+    r = requests.get(url, headers=headers)
+    r = r.json()
+
+    if 'error' not in r:
+        return r.get('data').get('subscribers')
+    return 0
